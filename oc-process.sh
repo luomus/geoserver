ed239d8d7e53a97b354a58dc440c9fb520874228
wkmor1 <willi@mmorris.email>
2024-05-02 14:42:10 +0300

    Update oc-process script

--- oc-process.sh
+++ oc-process.sh
@@ -3,11 +3,11 @@
 i="all"
 
 while getopts ":f:e:i::" flag; do
-case $flag in
-f) f=${OPTARG} ;;
-e) e=${OPTARG} ;;
-i) i=${OPTARG} ;;
-esac
+  case $flag in
+    f) f=${OPTARG} ;;
+    e) e=${OPTARG} ;;
+    i) i=${OPTARG} ;;
+  esac
 done
 
 set -a
@@ -20,29 +20,39 @@ BRANCH=$(git symbolic-ref --short -q HEAD)
 
 if [ "$BRANCH" != "main" ]; then
 
-DB_PASSWORD=$DEV_DB_PASSWORD
+  DB_PASSWORD=$DEV_DB_PASSWORD
+  GEOSERVER_DB_PASSWORD=$DEV_GEOSERVER_DB_PASSWORD
+  GEOSERVER_ADMIN_PASSWORD=$DEV_GEOSERVER_ADMIN_PASSWORD
 
 fi
 
-if [ $i = "volume" ]; then
+if [ $i = "volume-app" ]; then
 
-ITEM=".items[0]"
+  ITEM=".items[0]"
+
+elif [ $i = "volume-db" ]; then
+
+  ITEM=".items[1]"
 
 elif [ $i = "configmap" ]; then
 
-ITEM=".items[1]"
+  ITEM=".items[2]"
 
 elif [ $i = "secrets" ]; then
 
-ITEM=".items[2]"
+  ITEM=".items[3]"
+
+elif [ $i = "deploy-app" ]; then
+
+  ITEM=".items[4]"
 
 elif [ $i = "deploy-db" ]; then
 
-ITEM=".items[3]"
+  ITEM=".items[5]"
 
 elif [ $i = "service-db" ]; then
 
-ITEM=".items[4]"
+  ITEM=".items[6]"
 
 else
 
@@ -52,10 +62,17 @@ fi
 
 DB_USER=$(echo -n $DB_USER | base64)
 DB_PASSWORD=$(echo -n $DB_PASSWORD | base64)
+GEOSERVER_DB_USER=$(echo -n $GEOSERVER_DB_USER | base64)
+GEOSERVER_DB_PASSWORD=$(echo -n $GEOSERVER_DB_PASSWORD | base64)
+GEOSERVER_ADMIN_USER=$(echo -n $GEOSERVER_ADMIN_USER | base64)
+GEOSERVER_ADMIN_PASSWORD=$(echo -n $GEOSERVER_ADMIN_PASSWORD | base64)
 
 oc process -f $f \
--p BRANCH="$BRANCH" \
--p DB_PORT="$DB_PORT" \
--p DB_USER="$DB_USER" \
--p DB_PASSWORD="$DB_PASSWORD" \
-| jq $ITEM
+  -p BRANCH="$BRANCH" \
+  -p DB_USER="$DB_USER" \
+  -p DB_PASSWORD="$DB_PASSWORD" \
+  -p GEOSERVER_DB_USER="$GEOSERVER_DB_USER" \
+  -p GEOSERVER_DB_PASSWORD="$GEOSERVER_DB_PASSWORD" \
+  -p GEOSERVER_ADMIN_USER="$GEOSERVER_ADMIN_USER" \
+  -p GEOSERVER_ADMIN_PASSWORD="$GEOSERVER_ADMIN_PASSWORD" \
+  | jq $ITEM
