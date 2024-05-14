37767f7170587dc4841b458ff205a610ec83a0bc
wkmor1 <willi@mmorris.email>
2024-05-14 16:01:05 +0300

    Clean up script

--- backup.sh
+++ backup.sh
@@ -2,14 +2,14 @@
 
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
 
-echo "Archiving Geoserver data [$TIMESTAMP]\n"
+echo "Archiving Geoserver data [$TIMESTAMP]"
 
 tar -czvf backup-$TIMESTAMP-$BRANCH.tar.gz /opt/geoserver_data
 
-echo "Copying data to object store [$TIMESTAMP]\n"
+echo "Copying data to object store [$TIMESTAMP]"
 
 rclone copy "/opt/backup-$TIMESTAMP-$BRANCH.tar.gz" "default:$OBJECT_STORE"
 
-echo "Removing local archive [$TIMESTAMP]\n"
+echo "Removing local archive [$TIMESTAMP]"
 
 rm /opt/backup-$TIMESTAMP-$BRANCH.tar.gz
